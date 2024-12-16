9ccb81f0c5177b31b789adbf7c58f447b4b9db1b
TheTruEnd <ntdat1602@gmail.com>
2024-12-17 00:22:35 +0700

    init

--- src/controller/impl/PromotionManagementImpl.java
+++ src/controller/impl/PromotionManagementImpl.java
@@ -4,6 +4,23 @@ import controller.PromotionManagement;
 import model.Promotion;
 
 public class PromotionManagementImpl implements PromotionManagement {
+	
+	PromotionDAO promotionDAO;
+	
+    public UserManagementImpl() {
+        promotionDAO = new PromotionDAO(); // Khởi tạo đối tượng PromotionDAO
+    }
+
+    public List<User> getAllUser() {
+        List<User> result = null;
+        try {
+            result = userDAO.getAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+	
     @Override
     public boolean addPromotion(Promotion promotion) {
         return false;
